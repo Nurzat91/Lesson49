7953538fc49dc0b8999778701ee7a198599219cb
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-10-12 11:38:03 +0300

    final result

--- task2/board.ts
+++ task2/board.ts
@@ -1,14 +1,17 @@
-for (let i: number = 0; i < 8; i++) {
-    let results:string = '';
+const task = () =>{
+    for (let i: number = 0; i < 8; i++) {
+        let results:string = '';
 
-    for (let j: number = 0; j < 8; j++) {
-        const count: boolean = (i + j) % 2 === 0;
+        for (let j: number = 0; j < 8; j++) {
+            const count: boolean = (i + j) % 2 === 0;
 
-        if (count) {
-            results += '  ';
-        } else {
-            results += '██';
+            if (count) {
+                results += '  ';
+            } else {
+                results += '██';
+            }
         }
+        console.log(results);
     }
-    console.log(results);
-}
+};
+task();
